7f5c5fae802beb80feafa3e783461d3571f7351d
Jonathan Berglind <jonatber@kth.se>
2015-05-06 13:41:09 +0200

    Cup "finished"
    Added Cup.java and finished it according to the class diagram.

---
+++ src/beerpong/Cup.java
@@ -0,0 +1,31 @@
+package beerpong;
+
+/**
+ * Class representing a cup at a beerpong-table.
+ * Created by jonathan on 2015-05-06.
+ */
+public class Cup {
+    private boolean isHit;
+
+    /**
+     * New cup with false assigned to isHit.
+     */
+    public Cup() {
+        isHit = false;
+    }
+
+    /**
+     * Returns a boolean with the status of the cup.
+     * @return true if hit, false otherwise.
+     */
+    public boolean status(){
+        return isHit;
+    }
+
+    /**
+     * Marks the cup as hit.
+     */
+    public void hit(){
+        isHit = true;
+    }
+}
