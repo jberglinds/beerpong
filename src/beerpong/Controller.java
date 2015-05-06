082e5e79a3ad43a416f510d6282ac0db2ca0ce9b
Jonathan Berglind <jonatber@kth.se>
2015-05-06 18:34:00 +0200

    Strobe easter egg added

--- src/beerpong/Controller.java
+++ src/beerpong/Controller.java
@@ -1,6 +1,21 @@
 package beerpong;
 
+import javafx.animation.FadeTransition;
+import javafx.event.Event;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+
+
 public class Controller {
 
+    public AnchorPane bg;
 
+    public void strobe(Event event) {
+        FadeTransition ft = new FadeTransition(Duration.millis(10), bg);
+        ft.setFromValue(1.0);
+        ft.setToValue(0);
+        ft.setCycleCount(100);
+        ft.setAutoReverse(true);
+        ft.play();
+    }
 }
