fdf4d98ca3bec191896f22c011a0d7b2ce587e79
Jonathan Berglind <jonatber@kth.se>
2015-05-06 16:02:18 +0200

    gameView start

--- src/beerpong/Main.java
+++ src/beerpong/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("gameView.fxml"));
         primaryStage.setTitle("Beer Pong LIVE");
-        primaryStage.setScene(new Scene(root, 700, 400));
+        primaryStage.setScene(new Scene(root, 800, 400));
         primaryStage.show();
     }
 
