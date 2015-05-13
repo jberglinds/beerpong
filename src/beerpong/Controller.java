cf26a2872b708ef328ba5fe71615d8d03eb8a3b0
Emil Lindblom <emillindblom95@gmail.com>
2015-05-13 14:34:13 +0200

    Lyckats skapa popup för att välja teamsize.
    Vi får ut en sträng när man klickar på ”Done” beroende på vilket
    alternativ som är ikryssat.
    
    Den strängen ska kunna användas på något sätt i Controller men jag
    kommer inte på hur.

--- src/beerpong/Controller.java
+++ src/beerpong/Controller.java
@@ -2,9 +2,13 @@ package beerpong;
 
 import javafx.animation.FadeTransition;
 import javafx.event.Event;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
 
@@ -15,6 +19,7 @@ public class Controller {
     public Label team1score, team2score;
     public Circle oneRight, twoRight, threeRight, fourRight, fiveRight, sixRight, sevenRight, eightRight, nineRight, tenRight;
     public Circle oneLeft, twoLeft, threeLeft, fourLeft, fiveLeft, sixLeft, sevenLeft, eightLeft, nineLeft, tenLeft;
+    public Label team1name;
 
     private boolean bounce;
 
@@ -26,14 +31,14 @@ public class Controller {
         teams = new Team[2];
 
         Player[] p = new Player[2];
-        p[0] = new Player("swag");
-        p[1] = new Player("swagg");
-        Team teamOne = new Team("teamOne", p);
+        p[0] = new Player("Emil");
+        p[1] = new Player("Jonathan");
+        Team teamOne = new Team("Dickteam", p);
 
         Player[] p2 = new Player[2];
-        p2[0] = new Player("yolo");
-        p2[1] = new Player("yoloo");
-        Team teamTwo = new Team("teamTwo", p2);
+        p2[0] = new Player("Jonas");
+        p2[1] = new Player("Mutte");
+        Team teamTwo = new Team("Snoken", p2);
 
         teams[0] = teamOne;
         teams[1] = teamTwo;
@@ -78,6 +83,16 @@ public class Controller {
             System.out.println("It is time for " + teams[currentTeamIndex].getTeamName() + " to play");
         }
     }
-    
+
+    public void nameChange(Event event) {
+        Stage primaryStage = new Stage();
+        VBox vbox = new VBox();
+        ComboBox combo = new ComboBox();
+        combo.getItems().addAll("1v1", "2v2", "3v3");
+        combo.setPromptText("Amount of players on each team");
+        vbox.getChildren().add(combo);
+        primaryStage.setScene(new Scene(vbox, 400, 200));
+        primaryStage.show();
+    }
 
 }
--- src/beerpong/Main.java
+++ src/beerpong/Main.java
@@ -7,6 +7,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
@@ -16,11 +19,41 @@ public class Main extends Application {
 
     @Override
     public void start(final Stage primaryStage) throws Exception{
+
         Parent root = FXMLLoader.load(getClass().getResource("gameView.fxml"));
         primaryStage.setTitle("Beer Pong LIVE");
         primaryStage.setScene(new Scene(root, 800, 400));
         primaryStage.show();
 
+        final Stage secondaryStage = new Stage();
+        VBox vbox = new VBox();
+        Button b = new Button("Done");
+        final ChoiceBox choice = new ChoiceBox();
+        choice.getItems().addAll("1v1", "2v2", "3v3");
+
+        b.setOnAction(
+                new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        String s = choice.getValue().toString();
+                        System.out.println(s);
+                        secondaryStage.close();
+                    }
+                });
+
+        Text t = new Text("Choose the type of game you are playing");
+        t.setTranslateX(80);
+        t.setTranslateY(-40);
+        t.setScaleX(1.3);
+        t.setScaleY(1.3);
+        b.setTranslateX(171);
+        b.setTranslateY(100);
+
+        choice.setTranslateX(165);
+        choice.setTranslateY(50);
+        vbox.getChildren().addAll(choice, b, t);
+        secondaryStage.setScene(new Scene(vbox, 400, 200));
+        secondaryStage.show();
     }
 
     public static void main(String[] args) {
